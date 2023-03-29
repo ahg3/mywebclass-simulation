081e7a7fa44dbf82837965d8495c7681a2da35f0
ahg3 <78547748+ahg3@users.noreply.github.com>
2023-03-28 20:44:53 -0400

    Converted back to original file
    Still receiving Error: Process completed with exit code 1.

--- tests/mywebclass_title.spec.js
+++ tests/mywebclass_title.spec.js
@@ -1,9 +1,8 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 
 test('Should have MyWebClass.org title', async ({ page }) => {
   // Expect a title "to contain" a substring.
-  await page.goto('http://localhost:3000');
-  const pageTitle = await page.title();
-  await expect(pageTitle).toMatch('MyWebClass.org');
-});
+  await page.goto('http://localhost:3000')
+  await expect(page).toHaveTitle('MyWebClass.org')
+})
