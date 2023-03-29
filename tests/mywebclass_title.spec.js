d4aff33c627a4190ab047b880aba4dfae975ce34
ahg3 <78547748+ahg3@users.noreply.github.com>
2023-03-28 20:41:47 -0400

    Update 4 to mywebclass_title.spec.js

--- tests/mywebclass_title.spec.js
+++ tests/mywebclass_title.spec.js
@@ -2,13 +2,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Should have MyWebClass.org title', async ({ page }) => {
-  // Navigate to the page and wait for it to load.
-  await page.goto('http://localhost:3000', { timeout: 10000 });
-
-  // Wait for the page to stabilize and for any animations to complete.
-  await page.waitForTimeout(2000);
-
-  // Get the title of the page and check if it contains the expected substring.
+  // Expect a title "to contain" a substring.
+  await page.goto('http://localhost:3000');
   const pageTitle = await page.title();
   await expect(pageTitle).toMatch('MyWebClass.org');
 });
